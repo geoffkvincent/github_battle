e9887e4335b66bb40d5fa2f9e775f7982c3bcebe
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-24 16:17:24 -0700

    github api

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react')
 var PropTypes = require('prop-types')
+var api = require('../utils/api')
 
 function SelectedLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -29,10 +30,18 @@ class Popular extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  componentDidMount() {
+    api.fetchPopularRepos(this.state.selectedLanguage)
+      .then(function (repos) {
+        console.log(repos)
+      })
   }
 
   updateLanguage(lang) {
