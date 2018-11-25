e9b7dff7aa08907bd358389f401d8b11adbd06d4
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-24 18:09:06 -0700

    repo grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -29,8 +29,15 @@ function RepoGrid (props) {
           <div className='popular-rank'>#{ index + 1 }</div>
           <ul className='space-list-items'>
             <li>
-              <img></img>
+              <img
+                className='avatar'
+                src={repo.owner.avatar_url}
+                alt={'Avatar for ' + repo.owner.login}
+              />
             </li>
+            <li><a href={repo.html_url}>{repo.name}</a></li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count} stars</li>
           </ul>
         </li>
       })}
