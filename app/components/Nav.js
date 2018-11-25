1829a6c35c6815ad2158896bebfb5c1e131cf952
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-25 16:09:34 -0700

    navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,7 +1,23 @@
 var React = require('react')
+var NavLink = require('react-router-dom').NavLink
 
 function Nav () {
   <ul className='nav'>
+    <li>
+      <NavLink activeClassName='active' to='/'>
+        Home
+      </NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to='/battle'>
+        Battle
+      </NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to='/popular'>
+        Popular
+      </NavLink>
+    </li>
   </ul>
 }
 
