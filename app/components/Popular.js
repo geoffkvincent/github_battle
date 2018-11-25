13505b7b0ad8ea09f143236e988ce39e48a116a3
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-24 18:11:00 -0700

    repo grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -25,21 +25,23 @@ function RepoGrid (props) {
   return (
     <ul className='popular-list'>
       {props.repos.map(function (repo, index) {
-        <li key={repo.name} className='popular-item'>
-          <div className='popular-rank'>#{ index + 1 }</div>
-          <ul className='space-list-items'>
-            <li>
-              <img
-                className='avatar'
-                src={repo.owner.avatar_url}
-                alt={'Avatar for ' + repo.owner.login}
-              />
-            </li>
-            <li><a href={repo.html_url}>{repo.name}</a></li>
-            <li>@{repo.owner.login}</li>
-            <li>{repo.stargazers_count} stars</li>
-          </ul>
-        </li>
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{ index + 1 }</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
       })}
     </ul>
   )
