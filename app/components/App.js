0904160d6cb4907f6f3b671617d743816dc34b77
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-26 16:24:55 -0700

    react router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,12 @@
 var React = require('react');
-var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
-var Nav = require('./Nav')
-var Home = require('./Home')
-var Battle = require('./Battle')
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Popular = require('./Popular');
 
 
 class App extends React.Component {
