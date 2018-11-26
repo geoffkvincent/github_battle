c1ee9aed0f36df90be69d793dab3d2fff64b580e
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-25 17:09:36 -0700

    navbar

--- app/components/App.js
+++ app/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       <Router>
         <div className='container'>  
           <Nav />    
-          <Route path='/' component={Popular} />
+          <Route path='/popular' component={Popular} />
         </div>
       </Router>   
     )
