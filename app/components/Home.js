22ed360f30817f5b04085db214c19958c5ff3e38
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-25 17:18:19 -0700

    home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,14 @@
 var React = require('react')
 
 class Home extends React.Component {
-
+  render() {
+    return (
+      <div className='home-container'>
+        <h1>Github Battle: Battle your friends...and stuff.</h1>
+        
+      </div>
+    )
+  }
 }
 
+module.exports = Home
