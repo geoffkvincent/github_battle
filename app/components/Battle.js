db4284489fa90613830ae668e4316765c27ef781
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-27 20:32:43 -0700

    update state

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,27 @@
 var React = require('react')
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null 
+    }
+    
+    this.handleSubmit = this.handleSubmit(this)
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function() {
+      var newState = {}
+      newState[id + 'Name'] = username
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
+    })
+  }
+    
   render() {
     return (
       <div>
