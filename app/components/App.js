fd0b3e80edbe91624c88300eeacafe55ccc482b0
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-02 18:50:18 -0700

    api battle functions

--- app/components/App.js
+++ app/components/App.js
@@ -6,7 +6,7 @@ var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
-var Results = require('/Results');
+var Results = require('./Results');
 var Popular = require('./Popular');
 
 
@@ -19,7 +19,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
-            <Route path='/battle/results' cpmponent={Results} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
