e6a2f81635ba8dda8189469dcb7e72f0a7cd295f
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-07 16:36:48 -0700

    loading comp

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ var queryString = require('query-string')
 var api = require('../utils/api')
 var Link = require('react-router-dom').Link
 var PlayerPreview = require('./PlayerPreview')
+var Loading = require('./Loading')
 
 function Profile (props) {
   var info = props.info
