721bb9791bc3b96cb8d27f148f111f27fe9fce39
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-07 16:38:18 -0700

    loading comp

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -98,7 +98,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos
-          ? <Loading />
+          ? <Loading text='DOWNLOADING'/>
           : <RepoGrid repos={this.state.repos} />
         }       
       </div>
