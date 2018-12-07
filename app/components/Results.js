907119af9ec29db789e693a6c1645bdc242e1c8f
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-06 21:10:02 -0700

    profile component

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,22 @@ var api = require('../utils/api')
 var Link = require('react-router-dom').Link
 var PlayerPreview = require('./PlayerPreview')
 
+function Profile (props) {
+  return (
+    <PlayerPreview username={info.login} avatar={info.avatar_url}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
 function Player (props) {
   return (
     <div>
