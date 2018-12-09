cafe76c6d28881cfde5efe26a76e44de689398bc
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-09 15:09:43 -0700

    es6 refactor

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,39 +1,39 @@
-var axios = require('axios')
+import axios from 'axios'
 
 var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var params = "?client_id=" + id + "&client_secret=" + sec;
 
-function getProfile (username) {
+getProfile = (username) => {
   return axios.get('https://api.github.com/users/' + username + params)
     .then(function (user) {
       return user.data
     })
 }
 
-function getRepos (username) {
+getRepos = (username) => {
   return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
 }
 
-function getStarCount (repos) {
+getStarCount = (repos) => {
   return repos.data.reduce(function (count, repo) {
     return count + repo.stargazers_count
   }, 0)
 }
 
-function calculateScore (profile, repos) {
+calculateScore = (profile, repos) => {
   var followers = profile.followers
   var totalStars = getStarCount(repos)
 
   return (followers * 3) + totalStars
 }
 
-function handleError (error) {
+handleError = (error) => {
   console.warn(error)
   return null
 }
 
-function getUserData (player) {
+getUserData = (player) => {
   return axios.all([
     getProfile(player),
     getRepos(player)
@@ -48,24 +48,24 @@ function getUserData (player) {
   })
 }
 
-function sortPlayers (players) {
+sortPlayers = (players) => {
   return players.sort(function (a,b) {
     return b.score - a.score
   })
 }
 
 module.exports = {
-  battle: function (players) {
+  battle: players = () => {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError)
   },
 
-  fetchPopularRepos: function (language) {
+  fetchPopularRepos: language = () => {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
       return axios.get(encodedURI)
-        .then(function (response){
+        .then(response => {
           return response.data.items;
         })
   }
